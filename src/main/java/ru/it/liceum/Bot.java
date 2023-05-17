807d82eec19f4e2d731fc4aaac1fb77d93d483ec
Aarenoksitolikus <super.naxa2001@yandex.ru>
2023-05-17 04:51:15 +0300

    dsdfngfdasd

--- src/main/java/ru/it/liceum/Bot.java
+++ src/main/java/ru/it/liceum/Bot.java
@@ -43,14 +43,10 @@ public class Bot extends TelegramLongPollingBot {
         Message message = update.getMessage();
         if (message.isCommand()) {
             switch (message.getText()) {
-                case "/check@ClassroomNoiseBot" -> checkNoiseLevelToChatRoom(message);
-                case "/music@ClassroomNoiseBot" -> playMusicToChatRoom(message);
-                case "/info@ClassroomNoiseBot" -> sendInfoToChatRoom(message);
-                case "/info" -> sendInfoToUser(message);
-                case "/music" -> playMusicToUser(message);
-                case "/check" -> checkNoiseLevelToUser(message);
-                case "/noise" -> sendText(message.getFrom().getId(), "В комнате слишком шумно. Сообщите об этом в чате класса");
-                case "/noise@ClassroomNoiseBot" -> sendText(message.getChatId(), "Пожалуйста, ведите себя потише!");
+                case "/check" -> checkNoiseLevelToChatRoom(message);
+                case "/music" -> playMusicToChatRoom(message);
+                case "/info" -> sendInfoToChatRoom(message);
+                case "/noise" -> sendText(message.getChatId(), "Пожалуйста, ведите себя потише!");
                 default -> sendText(message.getFrom().getId(), "Unavailable command");
             }
         } else if (message.getChat().isUserChat()){
@@ -58,30 +54,13 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    private void checkNoiseLevelToUser(Message message) {
-        sendText(message.getFrom().getId(), "Ссылка на сайт для проверки уровня громкости в помещении: https://youlean.co/online-loudness-meter/\n\nЕсли шкала находится не в зеленой зоне, значит, в комнате слишком шумно.");
-    }
-
-    private void playMusicToUser(Message message) {
-        sendText(message.getFrom().getId(), "Ссылка на сайт с фоновой музыкой: https://noises.online");
-    }
-
-    private void sendInfoToUser(Message message) {
-        sendText(message.getFrom().getId(), """
-                Этот бот умеет отдавать ссылку на сайт, где можно проверить текущий уровень шума в помещении, а также ссылку на сайт с разнообразной фоновой расслабляющей музыкой.
-
-                Для получения ссылки на сайт с проверкой шума введите команду /check.
-
-                Для получения ссылки на сайт с фоновой музыкой, введите команду /music.""");
-    }
-
     private void sendInfoToChatRoom(Message message) {
         sendText(message.getChatId(), """
                 Этот бот умеет отдавать ссылку на сайт, где можно проверить текущий уровень шума в помещении, а также ссылку на сайт с разнообразной фоновой расслабляющей музыкой.
 
-                 Для получения ссылки на сайт с проверкой шума введите команду /check
+                Для получения ссылки на сайт с проверкой шума введите команду /check.
 
-                 для получения ссылки на сайт с фоновой музыкой, введите команду /music""");
+                Для получения ссылки на сайт с фоновой музыкой, введите команду /music""");
     }
 
     private void checkNoiseLevelToChatRoom(Message message) {
